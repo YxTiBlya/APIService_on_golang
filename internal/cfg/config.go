be8c87bf26d0753fde3f48562dc0d247e5944712
YxTiBlya <karasevstepan674@gmail.com>
2023-01-04 14:08:37 +0400

    add more vars

--- internal/cfg/config.go
+++ internal/cfg/config.go
@@ -5,12 +5,15 @@ import "time"
 type Config struct {
 	BindAddr    string        `toml:"bind_addr"`
 	SecretKey   string        `toml:"secret_key"`
+	JWTtime     time.Duration `toml:"jwt_time"`
 	DatabaseURL string        `toml:"database_url"`
 	RedisHost   string        `toml:"redis_host"`
 	RedisPort   string        `toml:"redis_port"`
 	RedisPass   string        `toml:"redis_pass"`
 	RedisDb     int           `toml:"redis_db"`
 	RedisExp    time.Duration `toml:"redis_exp"`
+	RabbitURL   string        `toml:"rabbit_url"`
+	MailerExp   time.Duration `toml:"mailer_exp"`
 }
 
 var config *Config
@@ -19,11 +22,13 @@ var config *Config
 func NewConfig() *Config {
 	config = &Config{
 		BindAddr:  ":8080",
+		JWTtime:   60,
 		SecretKey: "e5je5seiop34[0",
 		RedisPort: ":6379",
 		RedisPass: "",
 		RedisDb:   0,
 		RedisExp:  60,
+		MailerExp: 60,
 	}
 	return config
 }
