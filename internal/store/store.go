9b08fcec392cbaf00fc450bc97e1feef30fa3198
YxTiBlya <karasevstepan674@gmail.com>
2023-01-06 14:20:35 +0400

    add the convert pg config to dsn for create conn

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,6 +1,9 @@
 package store
 
 import (
+	"fmt"
+
+	"github.com/yxtiblya/internal/cfg"
 	"github.com/yxtiblya/internal/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -9,9 +12,11 @@ import (
 var DB *gorm.DB
 
 // create db conntection and return *gorm.DB, error
-func NewDB(database_url string) (*gorm.DB, error) {
+func NewDB(c *cfg.Config) (*gorm.DB, error) {
 	var err error
-	DB, err = gorm.Open(postgres.Open(database_url), &gorm.Config{})
+	dsn := fmt.Sprintf("user=%v password=%v host=%v port=%v dbname=%v sslmode=%v", c.Postgres_user, c.Postgres_password, c.Postgres_host, c.Postgres_port, c.Postgres_dbname, c.Postgres_ssl)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
