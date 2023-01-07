93922eb87e20bd82021617575c3cf23a44d67fa6
YxTiBlya <karasevstepan674@gmail.com>
2023-01-07 22:24:40 +0400

    add keyboard buttons and logic for update token btn

--- cmd/telegram/app.py
+++ cmd/telegram/app.py
@@ -1,7 +1,10 @@
-import logging
+import logging, requests
+import config as cfg
 from aiogram import Bot, Dispatcher, executor, types
+import markup as nav
 
 
+# init logger
 logger = logging.getLogger("telegram-bot")
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler("./logger/telegram.log", mode="a")
@@ -9,18 +12,34 @@ console_handler = logging.StreamHandler()
 formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
 
 file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
-token = ""
+# get config
+config = cfg.get_config()
 
-bot = Bot(token)
+# init bot and dispatcher
+bot = Bot(config.Token)
 dp = Dispatcher(bot)
-
 
 @dp.message_handler(commands=['start'])
 async def start(message:types.Message):
     if message.chat.type == 'private':
-        #await bot.send_message(message.chat.id, "Привет👋")
-        #await bot.send_message(message.chat.id, 'Выбери сервис ниже.')
-        pass
+        if message.from_user.id in config.Admins:
+            await bot.send_message(message.chat.id, "Добро пожаловать!", reply_markup=nav.main_menu)
+        else:
+            await bot.send_message(message.chat.id, "Отказано")
+
+@dp.message_handler()
+async def main(message:types.Message):
+    if message.chat.type == 'private':
+        if message.text == "Обновить токен":
+            r = requests.get(f"{config.Addr}/api/getJWT")
+            config.update_jwt(r.json()['token'])
+
+            await bot.send_message(message.chat.id, "Успешно")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates = True) 
