0f2b31065ecb98f76d9eb646a0661a4b96f2344c
YxTiBlya <karasevstepan674@gmail.com>
2023-01-10 02:36:49 +0400

    add cancel btn

--- cmd/telegram/markup.py
+++ cmd/telegram/markup.py
@@ -6,3 +6,5 @@ messages = KeyboardButton("Сообщения")
 refr_token = KeyboardButton("Обновить токен")
 stat = KeyboardButton("Статистика")
 main_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(refr_token).row(contacts,mailing,messages).add(stat)
+
+cancel = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Отмена"))
